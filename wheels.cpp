55c03c04e6cf600024ecce6eb7cb3248a8844c32
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2024-02-12 13:20:02 +0530

    Update wheels.cpp

--- wheels.cpp
+++ wheels.cpp
@@ -66,16 +66,6 @@ const char* htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
       box-shadow: none; 
     }
 
-    .noselect {
-      -webkit-touch-callout: none; /* iOS Safari */
-        -webkit-user-select: none; /* Safari */
-         -khtml-user-select: none; /* Konqueror HTML */
-           -moz-user-select: none; /* Firefox */
-            -ms-user-select: none; /* Internet Explorer/Edge */
-                user-select: none; /* Non-prefixed version, currently
-                                      supported by Chrome and Opera */
-    }
-
     .slidecontainer {
       width: 100%;
     }
